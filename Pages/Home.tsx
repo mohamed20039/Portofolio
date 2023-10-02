6c30cf80e9bed0d80347bf143c969f8e5eebdabe
Mohamed Mohamud <mohammedmohamuud.01@gmail.com>
2023-10-02 15:30:07 -0700

    Started my portofolio

---
+++ Pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home is Home whether you like it or not</div>
+  )
+}
+
+export default Home
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Home from "@/Pages/Home";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <main className="">
+      <h1></h1>
+      <Home />
+    </main>
+  );
+}
