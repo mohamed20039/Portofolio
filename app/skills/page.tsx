88c52ad4ccd9c2a74ebfb01da72deb4d33a21796
Mohamed Mohamud <mohammedmohamuud.01@gmail.com>
2023-10-18 20:06:50 -0700

    Started Skills Page and made some design changes

---
+++ app/skills/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center min-h-screen'>
+      
+    </div>
+  )
+}
+
+export default Skills
