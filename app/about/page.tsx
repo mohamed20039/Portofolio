06150fab671056e6674d8362ec631caf9740270e
Mohamed Mohamud <mohammedmohamuud.01@gmail.com>
2023-10-19 12:42:53 -0700

    New Commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ const About = () => {
       </div>
       <div className='flex flex-col items-center md:order-2'>
         <p className='text-center mt-6 md:w-[600px] md:text-2xl md:mr-16'>
-          I'm a <span className='text-sky-400'>frontend developer</span> based in Somalia. I have a passion for creating beautiful and functional user interfaces, and I love bringing designer’s visions to life. As a frontend developer, I'm proficient in a variety of programming languages and tools, including HTML, CSS, JavaScript, and React. I've worked with a diverse range of projects.
+          I'm a <span className='text-sky-400'>&apos;frontend developer&apos;</span> based in Somalia. I have a passion for creating beautiful and functional user interfaces, and I love bringing designer’s visions to life. As a frontend developer, I'm proficient in a variety of programming languages and tools, including HTML, CSS, JavaScript, and React. I've worked with a diverse range of projects.
         </p>
       </div>
     </div>
