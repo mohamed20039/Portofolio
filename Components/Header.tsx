cfc1b508511ed78e377d7108ab9ba62889655f1a
Mohamed Mohamud <mohammedmohamuud.01@gmail.com>
2023-10-19 17:51:12 -0700

    Fixing bugs

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -3,22 +3,22 @@ import React from 'react';
 import { links } from '@/Data';
 
 const Header = () => {
-    return (
-      <div className="text-white flex justify-center items-center fixed top-0 left-0 w-full h-16 z-50">
-        <nav className="bg-white text-black py-3 px-4 rounded-md">
-          <ul className="flex gap-4 justify-center items-center">
-            {links.map((link, index) => (
-              <li key={index}>
-                <Link href={link.hash}>
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    );
-  };
+  return (
+    <div className="text-white flex justify-center items-center fixed top-0 left-0 w-full h-16 z-50">
+      <nav className="bg-white text-black py-3 px-4 rounded-md">
+        <ul className="flex gap-4 justify-center items-center">
+          {links.map((link, index) => (
+            <li key={index}>
+              <Link href={link.hash}>
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
-  export default Header
+export default Header;
     
