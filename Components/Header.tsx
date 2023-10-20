9c166b9ee1bcc7b40b70e064fd9071ff48330848
Mohamed Mohamud <mohammedmohamuud.01@gmail.com>
2023-10-19 22:28:44 -0700

    Made some changes

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -5,7 +5,7 @@ import { links } from '@/Data';
 const Header = () => {
   return (
     <div className="text-white flex justify-center items-center fixed top-0 left-0 w-full h-16 z-50">
-      <nav className="bg-white text-black p-3 rounded-md">
+      <nav className="bg-white text-black p-4 rounded-md">
         <ul className="flex gap-4 justify-center items-center">
           {links.map((link, index) => (
             <li key={index}>
--- Components/HomeHero.tsx
+++ Components/HomeHero.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image'
 const HomeHero = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-        <div className='mt-40 text-center flex flex-col justify-center items-center space-y-4'>
-          <Image src={Me} alt="My Picture" width={300} height={500} className="rounded-full" />
-          <p className="font-medium">Hi, I'm Mohamed 👋</p>
-          <p className="text-center text-2xl font-medium md:w-[540px]">
-            Passionate Frontend Developer with a Focus on Creating Seamless User Experiences
-          </p>
+        <div className='mt-40 text-center flex flex-col justify-center items-center'>
+        <Image src={Me} alt="My Picture" width={300} height={500} className="rounded-full" />
+        <p className="mt-5 font-medium">Hi, I'm Mohamed 👋</p>
+        <p className="text-center text-2xl font-medium md:w-[540px] mt-5">
+          Passionate Frontend Developer with a Focus on Creating Seamless User Experiences
+        </p>
         </div>
     </div>
   );
